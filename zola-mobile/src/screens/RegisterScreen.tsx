f6fb890a5b123504b4b742365085f0cb7c145f16
Gia-Bao-2-0-0-3 <baoliengianguyen@gmail.com>
2025-04-06 17:27:43 +0700

    trang dang ky

--- zola-mobile/src/screens/RegisterScreen.tsx
+++ zola-mobile/src/screens/RegisterScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, View,Modal, Text, TextInput,Pressable, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -9,9 +9,14 @@ type RegisterScreenProps = {
 
 const RegisterScreen = ({ navigation }: RegisterScreenProps) => {
   const [phoneNumber, setPhoneNumber] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(false);
+  const [showOTPModal, setShowOTPModal] = useState(false);
+
+  const handleRegister = () => {
+    setShowOTPModal(true);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="dark" />
@@ -28,14 +33,11 @@ const RegisterScreen = ({ navigation }: RegisterScreenProps) => {
           >
             <Text style={styles.backButtonText}>←</Text>
           </TouchableOpacity>
-          <Text style={styles.headerTitle}>Tạo tài khoản</Text>
+          
           <View style={styles.placeholder} />
         </View>
-        
-        {/* Logo */}
-        <View style={styles.logoContainer}>
-          <Text style={styles.logoText}>Zola</Text>
-        </View>
+        <Text style={styles.headerTitle}>Nhập số điện thoại</Text>
+      
         
         {/* Form */}
         <View style={styles.form}>
@@ -46,36 +48,88 @@ const RegisterScreen = ({ navigation }: RegisterScreenProps) => {
             onChangeText={setPhoneNumber}
             keyboardType="phone-pad"
           />
-          
-          <TextInput
-            style={styles.input}
-            placeholder="Tên hiển thị"
-            value={name}
-            onChangeText={setName}
-          />
-          
-          <TextInput
-            style={styles.input}
-            placeholder="Mật khẩu"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry
-          />
-          
+
           <TouchableOpacity 
-            style={[styles.registerButton, phoneNumber && name && password ? styles.registerButtonActive : null]}
-            disabled={!phoneNumber || !name || !password}
+            style={[styles.registerButton, phoneNumber  && checked1 && checked2? styles.registerButtonActive : null]}
+            disabled={!phoneNumber ||  checked1 === false || checked2 === false}
+            onPress={handleRegister}
           >
             <Text style={styles.registerButtonText}>Đăng ký</Text>
           </TouchableOpacity>
           
-          <View style={styles.termsContainer}>
-            <Text style={styles.termsText}>
-              Bằng việc đăng ký, bạn đồng ý với Điều khoản sử dụng và Chính sách bảo mật của Zola
+          <View style={styles.termsRow}>
+              <Pressable onPress={() => setChecked1(!checked1)} style={styles.checkbox}>
+                {checked1 && <Text style={styles.checkmark}>✔</Text>}
+              </Pressable>
+
+              <Text style={styles.label}>
+                Tôi đồng ý với
+                <Text style={styles.link} onPress={() => alert('Điều khoản')}>
+                   {' '}điều khoản sử dụng Zola
+                </Text>
+              </Text>
+              
+          </View>
+          <View style={styles.termsRow}>
+            <Pressable onPress={() => setChecked2(!checked2)} style={styles.checkbox}>
+              {checked2 && <Text style={styles.checkmark}>✔</Text>}
+            </Pressable>
+            <Text style={styles.label}>
+              Tôi đồng ý với
+              <Text style={styles.link} onPress={() => alert('Điều khoản')}>
+                {' '}điều khoản sử dụng mạng xã hội của Zola
+              </Text>
             </Text>
           </View>
+
         </View>
+        
+
       </KeyboardAvoidingView>
+          <Modal
+              visible={showOTPModal}
+              transparent
+              animationType="fade"
+            >
+              <View style={styles.modalContainer}>
+                <View style={styles.modalContent}>
+                  <Text style={styles.modalTitle}>Nhận mã xác thực qua số {'\n'} <Text style={styles.boldText}>{phoneNumber}?</Text></Text>
+                  <Text style={{ textAlign: 'center', marginBottom: 15,fontSize: 16 }}>
+                    Zola sẽ gửi mã xác thực qua số điện thoại này.
+                  </Text>
+                  <View style={{ flexDirection: 'column-reverse', gap: 10 }}>
+                    <TouchableOpacity 
+                      style={{
+                        paddingVertical: 12,
+                        alignItems: 'center'
+                      }}
+                      onPress={() => setShowOTPModal(false)}
+                    >
+                      <Text style={{ color: '#000', fontWeight: 'bold', fontSize: 16 }}>
+                        Đổi số khác
+                      </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity 
+                      style={{
+                        paddingVertical: 12,
+                        alignItems: 'center'
+                      }}
+                      onPress={() => {
+                        setShowOTPModal(false);
+                        navigation.navigate('OTP', { phoneNumber });
+                      }}
+                    >
+                      <Text style={{ color: '#0068FF', fontWeight: 'bold', fontSize: 16 }}>
+                        Tiếp tục
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+
+                
+                </View>
+              </View>
+          </Modal>
     </SafeAreaView>
   );
 };
@@ -101,17 +155,16 @@ const styles = StyleSheet.create({
   backButtonText: {
     fontSize: 24,
     fontWeight: 'bold',
+
   },
   headerTitle: {
-    fontSize: 18,
+    fontSize: 25,
     fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 30,
   },
   placeholder: {
     width: 40,
-  },
-  logoContainer: {
-    alignItems: 'center',
-    marginBottom: 40,
   },
   logoText: {
     fontSize: 60,
@@ -122,9 +175,11 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   input: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderWidth: 2,
+    borderColor: 'blue',
+    borderRadius: 10,
     paddingVertical: 15,
+    paddingHorizontal: 10,
     fontSize: 16,
     marginBottom: 20,
   },
@@ -145,6 +200,8 @@ const styles = StyleSheet.create({
   },
   termsContainer: {
     alignItems: 'center',
+    flexDirection: 'row',
+    margin: 10,
   },
   termsText: {
     textAlign: 'center',
@@ -152,6 +209,68 @@ const styles = StyleSheet.create({
     color: '#666',
     lineHeight: 18,
   },
+  termsRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    marginTop: 10,
+  },
+  checkbox: {
+    width: 24,
+    height: 24,
+    borderWidth: 2,
+    borderColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 8,
+  },
+  checkmark: {
+    color: 'blue',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  label: {
+    color: '#000',
+    fontSize: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  link: {
+    color: '#007AFF',
+    fontWeight: 'bold',
+    fontSize: 15,
+    textDecorationLine: 'underline',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 12,
+    width: '85%',
+    elevation: 5,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  modalButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    marginHorizontal: 5,
+  },
+ 
+  boldText: {
+    fontWeight: 'bold',
+  },
+  
 });
 
 export default RegisterScreen; 
