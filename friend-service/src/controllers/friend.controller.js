c514fc849bed8770ed6c29033a0d4d613c2791cd
Chippo156 <vovannghiahiep150623@gmail.com>
2025-04-13 14:10:09 +0700

    update message and code

--- friend-service/src/controllers/friend.controller.js
+++ friend-service/src/controllers/friend.controller.js
@@ -5,17 +5,23 @@ FriendController.findUserByPhoneNumber = async (req, res) => {
   const { phoneNumber } = req.query;
 
   if (!phoneNumber) {
-    return res.status(400).json({ message: "Số điện thoại không hợp lệ" });
+    return res
+      .status(400)
+      .json({ code: 400, message: "Số điện thoại không hợp lệ" });
   }
   try {
     const user = await FriendModel.findUserByPhoneNumber(phoneNumber);
     if (!user) {
-      return res.status(404).json({ message: "Số điện thoại không hợp lệ" });
+      return res
+        .status(404)
+        .json({ code: 404, message: "Số điện thoại không hợp lệ" });
     }
-    return res.status(200).json({ user });
+    return res.status(200).json({ code: 200, user });
   } catch (error) {
     console.error("Có lỗi khi tìm kiếm người dùng:", error);
-    return res.status(500).json({ message: "Có lỗi khi tìm kiếm người dùng" });
+    return res
+      .status(500)
+      .json({ code: 500, message: "Có lỗi khi tìm kiếm người dùng" });
   }
 };
 
